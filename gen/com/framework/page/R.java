04117eeaf313d19c253101cd38fd457b6681bae0
jeffreyzhang <shine8606@gmail.com>
2012-05-10 23:09:17 +0800

    添加几个 Activity

--- gen/com/framework/page/R.java
+++ gen/com/framework/page/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int start=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/framework/page/LoginActivity.java
+++ src/com/framework/page/LoginActivity.java
@@ -3,11 +3,11 @@ package com.framework.page;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class StartActivity extends Activity {
+public class LoginActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start);
+        setContentView(R.layout.login);
     }
 }
