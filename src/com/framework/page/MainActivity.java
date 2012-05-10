ad5d5d55fe3fa99ba86d14103ef61f0f5178a60a
haiyupeter <21haiyu21@163.com>
2012-05-11 01:27:39 +0800

    删除标题

--- src/com/framework/page/MainActivity.java
+++ src/com/framework/page/MainActivity.java
@@ -2,12 +2,14 @@ package com.framework.page;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
     }
 }
