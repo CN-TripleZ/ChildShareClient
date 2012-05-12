2c213290cfaacae48c25066af9f072aac41750fd
haiyupeter <21haiyu21@163.com>
2012-05-12 20:55:43 +0800

    增加基础类

---
+++ src/com/qinzi/constants/Url.java
@@ -0,0 +1,8 @@
+package com.qinzi.constants;
+/**
+ * @author chenhaiyu e-mail:haiyupeter@163.com
+ * @Version 2012-5-12 下午8:15:11
+ */
+public class Url {
+	public static String DOMAIN_URL = "http://haiyupeter.gicp.net:8080/"; 
+}
---
+++ src/com/qinzi/pojo/ItemInfo.java
@@ -0,0 +1,45 @@
+package com.qinzi.pojo;
+
+/**
+ * @author chenhaiyu e-mail:haiyupeter@163.com
+ * @Version 2012-5-12 下午6:52:40
+ */
+public class ItemInfo {
+	private String id;
+	private String text;
+	private String image;
+	private String time;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
